e5cc5674c9c1fde7d42f4261a5437eb606ac121e
hexists <hexists@daumkakao.com>
2016-06-08 21:04:57 +0900

    temp

---
+++ the-trigram/the-trigram.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+
+import sys
+
+
+def main(argv):
+    '''
+    https://www.hackerrank.com/challenges/the-trigram
+    '''
+    buf = raw_input()
+    words = buf.split()
+
+    trigrams = {}
+    for i in range(len(words)):
+        if words[i] 
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
