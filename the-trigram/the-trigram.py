bb1c71d4f3c66b5a0bdcbbb0bdca5983802a9a97
hexists <hexists@daum.net>
2016-06-08 21:55:41 +0900

    the-trigram

--- the-trigram/the-trigram.py
+++ the-trigram/the-trigram.py
@@ -3,18 +3,32 @@
 
 
 import sys
+import operator
+import re
 
 
 def main(argv):
     '''
     https://www.hackerrank.com/challenges/the-trigram
     '''
-    buf = raw_input()
-    words = buf.split()
+
+    reg = re.compile('[.\n]+')
+
+    words = ''
+    for buf in sys.stdin:
+      words += buf
+    words = reg.sub(' ', words).lower().split()
 
     trigrams = {}
-    for i in range(len(words)):
-        if words[i] 
+    for i in range(len(words) - 2):
+        trigram = ' '.join(words[i:i + 3]).strip()
+        if trigram not in trigrams:
+          trigrams[trigram] = 1
+        else:
+          trigrams[trigram] += 1
+
+    print max(trigrams.iteritems(), key=operator.itemgetter(1))[0]
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
